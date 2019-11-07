abd09bdfeb0577259ab6ce9e1f8379cd94d79e4e
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-07 03:29:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,14 @@ def assign_rooms
   end
 end
   
-
+def printer(attendees)
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
+end
 
 
 
