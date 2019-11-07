18c17eacb552c1f0d090643f6c617bff5974c633
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-07 03:28:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms
-  room = 1
+  room = 0
   attendees.collect do |name|
     "Hello, #{name}! You'll be assigned to room #{name}!"
     room +=  1
