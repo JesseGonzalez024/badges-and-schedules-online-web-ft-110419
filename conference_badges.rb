c241f6cc1dbff5c3160d91aaa2566e6442776df8
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-07 03:17:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,23 +11,5 @@ end
 
   
 
-def batch_badge_creator(speakers)
-  
-  
-  speakers.each { |name| return b
-  end
-  
-  
-  
-  
-  
-end
 
-def printer
   
-end
-
-def assign_rooms(speakers, room)
-  puts "Hello, #{speakers}! You'll be assigned to room #{number}!"
-  
-end
