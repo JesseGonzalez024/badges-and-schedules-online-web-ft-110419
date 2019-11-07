aaa340e3feb05fbd7ed600aeed674dc888639e19
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-07 03:10:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,7 @@
 # Write your code here.
 def conference_badges
 
-  def badge_maker(name)
-    puts "Hello, my name is #{name}."
-  end
+  
 
   return badge_maker("Ariel")
 end
@@ -17,6 +15,8 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(speakers)
   speakers.each do |name| 
+  
+  
   end
   
   
